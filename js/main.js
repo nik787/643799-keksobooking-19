58e9068ca452fa024f4dc7d58315debb00922250
nik787 <nikito101d@gmail.com>
2020-01-31 00:59:39 +0300

    рандомное заполнение данных

--- js/main.js
+++ js/main.js
@@ -2,12 +2,36 @@
 
 var quantityObjects = 8;
 var PIN_WIDTH = 50;
+var LOCATION_MINY = 130;
+var LOCATION_MINX = 630;
 var MAP_WIDTH = 1200;
-var AD_TITLE = ['Квартира 1', 'Квартира 2', 'Квартира 3', 'Квартира 4', 'Квартира 5', 'Квартира 6', 'Квартира 7', 'Квартира 8'];
+var AD_AVATARS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+var AD_TITLE = ['Уютное гнездышко для молодоженов 1', 'Уютное гнездышко для молодоженов 2', 'Уютное гнездышко для молодоженов 3', 'Уютное гнездышко для молодоженов 4', 'Уютное гнездышко для молодоженов 5', 'Уютное гнездышко для молодоженов 6', 'Уютное гнездышко для молодоженов 7', 'Уютное гнездышко для молодоженов 8'];
+var AD_PRICE = 5200;
+var AD_TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var AD_CHECKIN = ['12:00', '13:00', '14:00'];
+var AD_CHECKOUT = ['12:00', '13:00', '14:00'];
+var AD_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var AD_DESCRIPTION = ['Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.'];
+var AD_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-function getRandomInt(min, max) {
+var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+var getRandomElementArr = function (arr) {
+  return arr[getRandomInt(0, arr.length)];
+};
+
+var getRAndomElementsArr = function (arr) {
+  var minElements = 1;
+  var newArr = [];
+  for (var i = 0; i < getRandomInt(minElements, arr.length); i++) {
+    newArr[i] = arr[i];
+  }
+  return newArr;
+};
+
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
@@ -15,38 +39,30 @@ map.classList.remove('map--faded');
 var createListAds = function () {
   var listAds = [];
   for (var i = 0; i < quantityObjects; i++) {
-    var location = {
-      x: getRandomInt(0, MAP_WIDTH),
-      y: getRandomInt(130, 630)
-    };
 
     listAds[i] = {
       author: {
-        avatar: 'img/avatars/user0' + getRandomInt(1, 8) + '.png',
+        avatar: getRandomElementArr(AD_AVATARS),
       },
 
       offer: {
         title: AD_TITLE[i], // строка, заголовок предложения
         address: location.x + ', ' + location.y, // строка, адрес предложения
-        price: '', // число, стоимость
-        type: '', // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
-        rooms: '', // число, количество комнат
-        guests: '', // число, количество гостей, которое можно разместить
-        checkin: '', // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-        checkout: '', // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-        features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-        description: '', // строка с описанием
-        photos: [
-          'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-          'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-          'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-        ]
+        price: AD_PRICE, // число, стоимость
+        type: getRandomElementArr(AD_TYPE), // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
+        rooms: getRandomInt(0, 5), // число, количество комнат
+        guests: getRandomInt(0, 5), // число, количество гостей, которое можно разместить
+        checkin: getRandomElementArr(AD_CHECKIN), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+        checkout: getRandomElementArr(AD_CHECKOUT), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+        features: getRAndomElementsArr(AD_FEATURES),
+        description: getRandomElementArr(AD_DESCRIPTION), // строка с описанием
+        photos: getRAndomElementsArr(AD_PHOTOS)
       },
 
       location: {
-        x: location.x, // случайное число, координата x метки на карте.
+        x: getRandomInt(0, MAP_WIDTH), // случайное число, координата x метки на карте.
         // Значение ограничено размерами блока, в котором перетаскивается метка = 1200
-        y: location.y // случайное число, координата y метки на карте от 130 до 630
+        y: getRandomInt(LOCATION_MINY, LOCATION_MINX) // случайное число, координата y метки на карте от 130 до 630
       }
     };
   }
